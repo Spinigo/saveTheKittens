4fee99ae4fd5bc3e1fb703d0bbe0baf57b78cf3f
Spinigo <Spinigo@users.noreply.github.com>
2017-04-24 12:33:44 +0200

    Adjust Print
    Print to print

--- SaveTheKittens/ViewController.swift
+++ SaveTheKittens/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        Print("Hello kittens")
+        print("Hello kittens")
     }
 
     override func didReceiveMemoryWarning() {
